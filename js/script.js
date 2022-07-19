0204cedce798995f4e04d1e51e94c7d04f06dcaa
Szymon Kedra <s5zee@wp.pl>
2022-07-19 16:51:40 +0200

    split js code into functions

--- js/script.js
+++ js/script.js
@@ -1,12 +1,33 @@
-let amountElement = document.querySelector(".js-amount");
-let resultElement = document.querySelector(".js-result");
-let currencyElement = document.querySelector(".js-currency");
-let formElement = document.querySelector(".js-form");
+{
+    const calculateResult = (currency, amount) => {
+        return amount / currency;
+    };
 
-formElement.addEventListener("submit", (event)=> {
-    event.preventDefault();
-    let currency = currencyElement.value;
-    let amount = amountElement.value;
-    let result = amount/currency;
-    resultElement.innerText = result.toFixed(2);
-});
+    const updateResultText = (result) => {
+        const resultElement = document.querySelector(".js-result");
+        resultElement.innerText = result.toFixed(2);
+    }
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const amountElement = document.querySelector(".js-amount");
+        const currencyElement = document.querySelector(".js-currency");
+
+        const currency = currencyElement.value;
+        const amount = amountElement.value;
+
+        const result = calculateResult(currency, amount);
+
+        updateResultText(result);
+    };
+
+
+    const init = () => {
+        const formElement = document.querySelector(".js-form");
+
+        formElement.addEventListener("submit", onFormSubmit);
+    };
+
+    init();
+}
